27141687c0a0a8bca531f0a6ab22b65f790d1910
wugelis <wergelis@msn.com>
2021-08-30 12:27:41 +0800

    1. 增加 Business Object Project

--- Simplo.Web.PlatformApp1/Global.asax.cs
+++ Simplo.Web.PlatformApp1/Global.asax.cs
@@ -1,13 +1,13 @@
-using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace Simplo.Web.PlatformApp1
 {
@@ -21,7 +21,7 @@ namespace Simplo.Web.PlatformApp1
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             //無論如何都使用 JSON 回傳.
             GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
-            //使用JS駝峰式命名
+			 //使用JS駝峰式命名
             var jsonformatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
             jsonformatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
